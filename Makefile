c64b41d99349bbebb4f8dee1fc8389ebdc7aae18
bowenislandsong <bowenislandsong@gmail.com>
2020-08-28 17:24:13 -0400

    (chore) Makefile and Setup

---
+++ Makefile
@@ -0,0 +1,29 @@
+
+KIND_CLUSTER_CONFIG := ./deploy/kind_cluster.yaml
+CODEGEN := ./hack/update-codegen.sh
+VERIFY := ./hack/verify-codegen.sh
+
+.PHONY: vendor
+vendor:
+	go mod tidy && go mod vendor
+
+# Generate deepcopy, conversion, clients, listers, and informers
+codegen:
+	# Clients, listers, and informers
+	$(CODEGEN)
+
+# Generates everything.
+.PHONY: gen-all
+gen-all: codegen vendor
+
+.PHONY: verify
+verify:
+	$(VERIFY)
+
+.PHONY: run-local
+run-local:
+	./hack/install_local.sh
+
+.PHONY: clean-e2e
+clean-e2e:
+	kubectl delete crds --all
---
+++ hack/install_local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if ! command -v kubectl &> /dev/null
+then
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    sudo mv kubectl /bin/
+fi
+
+if ! command -v kind &> /dev/null
+then
+    curl -sLo kind "$(curl -sL https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r '[.assets[] | select(.name == "kind-linux-amd64")] | first | .browser_download_url')"
+    chmod +x kind
+    sudo mv kind /bin/
+    kind create cluster
+fi
+
+
+kind create cluster --config ./hack/kind_cluster.yaml
+kubectl cluster-info --context kind-kind
+
+kubectl apply -f ./artifacts/examples/crd.yaml
+kubectl wait --timeout=5m --for=condition=Established crd $(kubectl get crd --output=jsonpath='{.items[*].metadata.name}')
